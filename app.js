2cbf0979c04e072ebcd134c66fdee62705cc6cc7
jungdw0624 <jdw8998@daum.net>
2020-07-07 09:02:37 +0900

    working on...

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const jamsilBr = document.getElementById('jamsilBr');
+const jamsilSr = document.getElementById('jamsilSr');
 const attendance = document.getElementById('attendance');
 const absence = document.getElementById('absence');
 
+///////// 버튼 이벤트 관련 method /////////
+for(i=0;i<jamsilBr.length;++i){
+    jamsilBr[i].addEventListener('click', () => {
+		console.log(jamsilBr[i].value)
+    })
+}
