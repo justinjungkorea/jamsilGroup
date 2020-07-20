c6f38fbd3a905b181619677da2eb651f5038349c
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-07-21 00:20:00 +0900

    html done!

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
+const leaders = document.getElementsByClassName('leader');
 const jamsilBr = document.getElementsByClassName('jamsilBr');
 const jamsilSr = document.getElementsByClassName('jamsilSr');
+const attendanceLeader = document.getElementById('attendanceLeader');
 const attendanceBr = document.getElementById('attendanceBr');
 const attendanceSr = document.getElementById('attendanceSr');
 const absence = document.getElementById('absence');
 
 let currentBrs = [];
 let currentSrs = [];
+let currentLeaders = [];
 
 ///////// 버튼 이벤트 관련 method /////////
-for(i=0;i<jamsilBr.length;++i){
+for(var i=0; i<leaders.length; ++i){
+  const leader = leaders[i].innerText;
+  const buttonId = leaders[i].id;
+
+  leaders[i].addEventListener('click', () => {
+    const temp = currentLeaders.indexOf(leader);
+    if(temp>=0){
+      document.getElementById(buttonId).style.border = '2px solid #000000'
+      currentLeaders.splice(temp, 1);
+    } else {
+      document.getElementById(buttonId).style.border = '2px solid #ff0000'
+      currentLeaders.push(leader);
+    }
+    
+    attendanceLeader.innerText = currentLeaders.toString();
+  })
+}
+
+for(var i=0; i<jamsilBr.length; ++i){
   const brother = jamsilBr[i].innerText;
   const buttonId = jamsilBr[i].id;
+
   jamsilBr[i].addEventListener('click', () => {
     const temp = currentBrs.indexOf(brother);
     if(temp>=0){
@@ -25,9 +47,10 @@ for(i=0;i<jamsilBr.length;++i){
   })
 }
 
-for(i=0;i<jamsilSr.length;++i){
+for(var i=0; i<jamsilSr.length; ++i){
   const sister = jamsilSr[i].innerText;
   const buttonId = jamsilSr[i].id;
+  
   jamsilSr[i].addEventListener('click', () => {
     const temp = currentSrs.indexOf(sister);
     if(temp>=0){
